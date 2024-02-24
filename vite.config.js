af15aa415d41608499728d75a91d9f89cc60f710
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-24 16:14:55 +0530

    asdasdasdasd

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/auth': {
-        target: 'https://www.auth.jiospin.info/',
+        target: 'https://auth.jiospin.info',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/auth/, ''),
       },
     },
   },
-  plugins: [react()],
 });
