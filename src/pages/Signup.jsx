04c891bc29b3e10302a0a62ed85929789e27b068
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-24 14:48:04 +0530

    asdasd

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -132,7 +132,8 @@ const Signup = () => {
         try {
             const d = await fetch('http://auth.jiospin.info/auth/demofile',{
                 method:'GET',
-                // credentials:'include',
+                credentials:'include',
+                mode:'same-origin'
             })
             const d1 = await d.json();
             console.log("response  ------ ",d1)
