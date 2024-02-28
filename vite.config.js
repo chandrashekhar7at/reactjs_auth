4f1e37e0ff3736008cf2d731dc80610dba7dc47f
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-28 21:39:34 +0530

    added name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth': 'http://baseergaming.com',
+      '/auth': 'http://auth.baseergaming.com',
     },
   },
 });
